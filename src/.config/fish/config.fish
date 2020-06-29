b4fb8ee2e5967d356b7f6b79d0399193974d3da2
鈴木颯太 <k017c1356@it-neec.jp>
2020-06-29 17:50:13 +0900

    [add] tmux set

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -1,23 +1,6 @@
 set fish_theme agnoster
 
-# automatic start tmux
-if [[ ! -n $TMUX ]]; then
-  # get the IDs
-  ID="`tmux list-sessions`"
-  if [[ -z "$ID" ]]; then
-    #tmux new-session && exit
-    ID="(NO SESSIONS)"
-  fi
-  create_new_session="Create New Session"
-  ID="${create_new_session}\n$ID"
-  ID="`echo $ID | fzf | cut -d: -f1`"
-  if [[ "$ID" = "${create_new_session}" ]]; then
-    tmux new-session && exit
-  elif [[ -n "$ID" ]]; then
-    tmux attach-session -t "$ID" && exit
-  else
-    # Start terminal normally
-  fi
-fi
-
-
+# tmux の自動起動
+if test -z $TMUX
+  tmux new-session
+end
