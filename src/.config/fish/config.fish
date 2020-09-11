90334244666d0547ba5e2c38ec9a7fbf74cc0427
Sota Suzuki <k017c1356@it-neec.jp>
2020-09-11 23:38:38 +0900

    [fix] commandline was not refreshed

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -33,6 +33,7 @@ function select_ghq_repository
 	set ghq_path (ghq list | fzf --preview "cat --color=always --style=header,grid --line-range :80 (ghq root)/{}/README.*")
 	set repo_path (ghq root)"/"$ghq_path | echo ""
 	cd $repo_path
+	commandline -f repaint
 end
 
 function exa_flex_tree
