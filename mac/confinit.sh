ebd9e0422609e72fbbeb28920fed9c201dff072b
Sota Suzuki <hurin@tatikaze.com>
2024-06-16 16:42:25 +0900

    :sparkles: update mac config initializer

--- mac/confinit.sh
+++ mac/confinit.sh
@@ -4,8 +4,18 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 # tmux
 ln -sf $SCRIPT_DIR/.tmux.conf $HOME
 
-# vim
-ln -sf $SCRIPT_DIR/.vimrc $HOME
-
 # fish
 ln -sf $SCRIPT_DIR/.config/fish/config.fish $HOME/.config/fish/config.fish
+
+# nvim
+ln -sf $SCRIPT_DIR/.config/nvim/init.lua $HOME/.config/nvim/init.lua
+
+mkdir -p $HOME/.config/nvim/lua
+for file in $SCRIPT_DIR/.config/nvim/lua/*; do
+  ln -sf "$file" ~/.config/nvim/lua/
+done
+
+mkdir -p $HOME/.config/nvim/plugin
+for file in $SCRIPT_DIR/.config/nvim/plugin/*; do
+  ln -sf "$file" ~/.config/nvim/plugin/
+done
