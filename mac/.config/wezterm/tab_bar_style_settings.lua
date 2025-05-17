d99b153bf676bb2ae0f50bdb3ce52f4bf69294de
sota suzuki <hurin@tatikaze.com>
2025-05-17 17:27:44 +0900

    :+1: support wezterm version update

--- mac/.config/wezterm/tab_bar_style_settings.lua
+++ mac/.config/wezterm/tab_bar_style_settings.lua
@@ -1,27 +1,29 @@
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
-local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+-- Powerline風の矢印
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 
-return {
-  active_tab_left = wezterm.format({
-    { Background = { Color = "#0b0022" } },
-    { Foreground = { Color = "#2b2042" } },
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local is_active = tab.is_active
+  local edge_bg = "#0b0022"
+  local bg = is_active and "#ae8b2d" or "#1b1032"
+  local fg = "#ffffff"
+  local edge_fg = bg
+  local title = wezterm.truncate_right(tab.active_pane.title, max_width - 2)
+
+  return {
+    { Background = { Color = edge_bg } },
+    { Foreground = { Color = edge_fg } },
     { Text = SOLID_LEFT_ARROW },
-  }),
-  active_tab_right = wezterm.format({
-    { Background = { Color = "#0b0022" } },
-    { Foreground = { Color = "#2b2042" } },
+    { Background = { Color = bg } },
+    { Foreground = { Color = fg } },
+    { Text = " " .. title .. " " },
+    { Background = { Color = edge_bg } },
+    { Foreground = { Color = edge_fg } },
     { Text = SOLID_RIGHT_ARROW },
-  }),
-  inactive_tab_left = wezterm.format({
-    { Background = { Color = "#0b0022" } },
-    { Foreground = { Color = "#1b1032" } },
-    { Text = SOLID_LEFT_ARROW },
-  }),
-  inactive_tab_right = wezterm.format({
-    { Background = { Color = "#0b0022" } },
-    { Foreground = { Color = "#1b1032" } },
-    { Text = SOLID_RIGHT_ARROW },
-  }),
-}
+  }
+end)
+
+return config
